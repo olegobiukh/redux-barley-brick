5c570ac41fd7c3eb8eecd860ec1e5b1fc6e006d1
obiukh19 <oleg.obiukh@gmail.com>
2019-04-05 19:34:22 +0300

    Deploy 1

---
+++ build/precache-manifest.aed6eb1bf285d17055977985eb15f9b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux-barley-brick/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c965cea633af9b90ef46",
+    "url": "/redux-barley-brick/build/static/js/runtime~main.260e75a9.js"
+  },
+  {
+    "revision": "fbb441d5d78040c832f0",
+    "url": "/redux-barley-brick/build/static/js/main.3c1e0b9d.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/redux-barley-brick/build/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "fbb441d5d78040c832f0",
+    "url": "/redux-barley-brick/build/static/css/main.5f14abab.chunk.css"
+  },
+  {
+    "revision": "263ce322285d6de2e88e479d162c68c7",
+    "url": "/redux-barley-brick/build/index.html"
+  }
+];
